e209c1facf6aa64cbae0e6cc4beb64a7328d705f
pandaapo_work <1052156701@qq.com>
2018-10-12 17:52:50 +0800

    ratingselect部分

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -22,18 +22,24 @@
                 </div>
                 <div class="cartcontrol-wrapper">
                     <!-- 传入props中food参数 -->
-                    <cartcontrol :food="food"></cartcontrol>
+                    <cartcontrol :food="food" v-on:cart-add="foodCartAdd"></cartcontrol>
                 </div>
                 <!-- 给“加入购物车”渐隐的动画，以解决直接隐藏导致小球动画的起点位置获取不到的问题 -->
                 <transition name="fade">
                     <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
                 </transition>
             </div>
-        </div>
-        <split></split>
-        <div class="info">
-            <h1 class="title">商品信息</h1>
-            <p class="text">{{food.info}}</p>
+            <split v-show="food.info"></split>
+            <div class="info" v-show="food.info">
+                <h1 class="title">商品信息</h1>
+                <p class="text">{{food.info}}</p>
+            </div>
+            <split></split>
+            <div class="rating">
+                <h1 class="title">商品评价</h1>
+                <!-- 这里必须使用驼峰转中划线 -->
+                <ratingselect v-bind:select-type="selectType" v-bind:only-content="onlyContent" v-bind:desc="desc" v-bind:ratings="food.ratings"></ratingselect>
+            </div>
         </div>
     </div>
   </transition>
@@ -45,6 +51,11 @@
   import cartcontrol from 'components/cartcontrol/cartcontrol'
   import Vue from 'vue'
   import split from 'components/split/split';
+  import ratingselect from 'components/ratingselect/ratingselect'
+
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
 
   export default {
     props:{
@@ -55,13 +66,24 @@
     },
     data() {
         return {
-            showFlag: false
+            showFlag: false,
+            // 跟踪了一些传给子组件的变量
+            selectType: ALL,
+            onlyContent: true,
+            desc: {
+                all: '全部',
+                positive: '推荐',
+                negative: '吐槽'
+            }
         }
     },
     methods: {
         // 编程习惯：不可能被父组件调用的方法名前面加上下划线
         show() {
             this.showFlag = true;
+            // show()的时候再初始化一遍：因为该组件可能被多个组件引用。？？？疑问：没明白。
+            this.selectType = ALL;
+            this.onlyContent = true;
             // $nextTick以后才能保证DOM已经渲染，高度计算才能正确
             this.$nextTick(() => {
                 if(!this.scroll) {
@@ -81,16 +103,22 @@
             if(!event._constructed){
                 return;
             }
-            console.info("food.vue向父组件派发cart-add事件--->");
-            //提交名为'cart-add'的事件给父组件，实现<添加按钮>图标的DOM元素传给父组件，实现抛物线小球动画
+            console.info("food.vue的<加入购物车>向父组件派发cart-add事件--->");
+            //提交名为'cart-add'的事件给父组件，实现<添加购物车>图标的DOM元素传给父组件，实现抛物线小球动画
             this.$emit('cart-add', event.target);
             // 第一次点击的时候，将this.food的count置为1
             Vue.set(this.food, 'count', 1)
+        },
+        foodCartAdd(el){
+            console.info("food.vue监听到cart-add事件，调用foodCartAdd()方法--->");
+            console.info("food.vue的cartcontrol向父组件派发cart-add事件--->");
+            this.$emit('cart-add', el);
         }
     },
     components: {
         cartcontrol,
-        split
+        split,
+        ratingselect
     }
   }
 </script>
@@ -100,6 +128,7 @@
         position : fixed
         left : 0
         top : 0
+        // 因为底部有个购物车
         bottom : 48px
         // 该层小于底部的购物车，以及点击购物车的弹出层
         z-index : 30
@@ -121,7 +150,7 @@
             width : 100%
             // 解决图片异步加载带来的页面“抖动”：一开始将图片高度定好
             height : 0
-            // 使所在和“盒子”是个宽高相等的容器
+            // 使和所在“盒子”是个宽高相等的容器
             padding-top : 100%
             img
                 position : absolute
@@ -140,6 +169,7 @@
                     font-size: 20px
                     color: #fff
         .content
+            position : relative
             padding: 18px
             .title
                 line-height : 14px
@@ -169,26 +199,45 @@
                     text-decoration : line-through
                     font-size: 10px
                     color: rgb(147, 153, 159)
-        .cartcontrol-wrapper
-            position : absolute
-            right : 12px
-            bottom : 12px
-        .buy
-            position : absolute
-            right : 18px
-            bottom : 18px
-            // 盖住cartcontrol组件
-            z-index : 10
-            height : 24px
-            line-height : 24px
-            padding : 0 12px
-            box-sizing : border-box
-            border-radius : 10px
-            font-size : 10px
-            color: #fff
-            background-color : rgb(0, 160, 220)
-            transition : all 0.2s
-            opacity : 1
-            &.fade-enter, &.fade-leave-active
-                opacity : 0
+            .cartcontrol-wrapper
+                position : absolute
+                right : 12px
+                bottom : 12px
+            .buy
+                position : absolute
+                right : 18px
+                bottom : 18px
+                // 盖住cartcontrol组件
+                z-index : 10
+                height : 24px
+                line-height : 24px
+                padding : 0 12px
+                box-sizing : border-box
+                border-radius : 10px
+                font-size : 10px
+                color: #fff
+                background-color : rgb(0, 160, 220)
+                transition : all 0.2s
+                opacity : 1
+                &.fade-enter, &.fade-leave-active
+                    opacity : 0
+        .info
+            padding: 18px
+            .title
+                line-height : 14px
+                margin-bottom : 6px
+                font-size : 14px
+                color: rgb(7, 17, 27)
+            .text
+                line-height : 24px
+                padding : 0 8px
+                font-size : 12px
+                color: rgb(77, 85, 93)
+        .rating
+            padding-top : 18px
+            .title
+                line-height : 14px
+                margin-left : 18px
+                font-size : 14px
+                color: rgb(7, 17,27)
 </style>
---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="ratingselect">
+      <div class="rating-type">
+          <span>{{desc.all}}</span>
+          <span>{{desc.positive}}</span>
+          <span>{{desc.negative}}</span>
+      </div>
+      <div class="switch">
+          <span class="icon-check_circle"></span>
+          <span class="text">只看有内容的评价</span>
+      </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+    const POSITIVE = 0;
+    const NEGATIVE = 1;
+    const ALL = 2;
+
+  export default {
+    props: {
+        // 因为各面板的标题在不同情形下不一样，需要从外部传入
+        ratings: {
+            type: Array,
+            // 数组的对象需要返回函数，和其他变量有点区别
+            default() {
+                return [];
+            }
+        },
+        selectType: {
+            type: Number,
+            default: ALL
+        },
+        onlyContent: {
+            type: Boolean,
+            default: false
+        },
+        desc: {
+            type: Object,
+            default() {
+                return {
+                    all: '全部',
+                    positive: '满意',
+                    negative: '不满意'
+                }
+            }
+        }
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+</style>
